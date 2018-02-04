76f6d3b43f3cecdcb9f1422aca15c2c8814426a4
Samuel Ong <s10171663@connect.np.edu.sg>
2018-02-04 21:17:42 +0800

    Revert "Revert "help me :<""
    This reverts commit fe5abcdee3d6383969e548e0bdb98d9d7b9b51fa.

---
+++ AOOADSkeletonCode/Entities/Agent.cs
@@ -0,0 +1,28 @@
+﻿namespace AOOADSkeletonCode.Entities
+{
+    class Agent
+    {
+        private string _accNum;
+        private string _name;
+        private CustomerCollection _myCustomers = new CustomerCollection();
+        public CustomerCollection myCustomers
+        {
+            get { return _myCustomers; }
+        }
+        public Agent()
+        {
+
+        }
+
+        public decimal calculatePay()
+        {
+            decimal pay = 0 ;
+            return pay;
+        }
+
+        public void addCustomer(string accNum)
+        {
+            _myCustomers.addCustomer(accNum);
+        }
+    }
+}
---
+++ AOOADSkeletonCode/Interfaces/IPolicyIterator/PolicyIterator.cs
@@ -0,0 +1,39 @@
+﻿using AOOADSkeletonCode.Interfaces.IPolicyCollection;
+
+namespace AOOADSkeletonCode.Interfaces.IPolicyIterator
+{
+    class PolicyIterator : IPolicyIterator
+    {
+        protected PolicyCollection _collection;
+        protected int _currIndex;
+
+        public PolicyIterator(PolicyCollection collection)
+        {
+            _collection = collection;
+            _currIndex = -1;
+        }
+
+        virtual public bool HasNext()
+        {
+            if (_currIndex + 1 < _collection.Count())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        virtual public object Next()
+        {
+            _currIndex++;
+            //IMPLEMENTATION FOR FILTERING
+
+            //
+            return _collection.GetPolicy(_currIndex); // RETURN SOMETHING
+        }
+
+        public void Reset()
+        {
+            _currIndex = -1;
+        }
+    }
+}
